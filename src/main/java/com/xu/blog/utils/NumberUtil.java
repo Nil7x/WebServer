fd2f9177819d19e79fab8e5b7afaebcfc231894b
Nil7x <88566794+Nil7x@users.noreply.github.com>
2022-07-24 23:44:18 +0800

    Update NumberUtil.java

--- src/main/java/com/xu/blog/utils/NumberUtil.java
+++ src/main/java/com/xu/blog/utils/NumberUtil.java
@@ -3,12 +3,7 @@ package com.xu.blog.utils;
 import java.util.regex.Matcher;
 import java.util.regex.Pattern;
 
-/**
- * @author 13
- * @qq交流群 796794009
- * @email 2449207463@qq.com
- * @link http:13blog.site
- */
+
 public class NumberUtil {
 
     private NumberUtil() {
