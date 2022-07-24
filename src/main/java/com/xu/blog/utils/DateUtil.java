c5945322b517dcbb8380e4ff0f861f20143f3a77
Nil7x <88566794+Nil7x@users.noreply.github.com>
2022-07-24 23:43:21 +0800

    Update DateUtil.java

--- src/main/java/com/xu/blog/utils/DateUtil.java
+++ src/main/java/com/xu/blog/utils/DateUtil.java
@@ -3,12 +3,7 @@ package com.xu.blog.utils;
 import java.text.SimpleDateFormat;
 import java.util.Date;
 
-/**
- * @author 13
- * @qq交流群 796794009
- * @email 2449207463@qq.com
- * @link http:13blog.site
- */
+
 public class DateUtil {
 
     /**
